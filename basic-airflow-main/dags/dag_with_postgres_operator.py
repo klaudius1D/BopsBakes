6b34ad06c9652d8fc08dade4350a663a7623d3a8
Klaude Dungca <klaude.dungca@gmail.com>
2024-03-03 07:48:46 -0800

    updated postgres connection, attempt creating first dag

---
+++ basic-airflow-main/dags/dag_with_postgres_operator.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.postgres.operators.post import PosgresOperator
+
+
+default_arg = {
+    'owner': 'Czar Klaudius',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_postgres_operaor_v1.0'
+    default_args=default_args,
+    start_date=datetime(2021, 12, 19),
+    schedule_interval='0 0 * * *'
+) as dag:
+    task1 = PostgresOperator(
+        task_id='sample_query',
+        postgres_conn_id='postgres_default',
+         sql=''' 
+        CREATE TABLE IF NOT EXISTS dag_runs (
+            dt date,
+            dag_id character varying,
+            primary key (dt, dag_id)
+        ''',
+    )   
+    task1
