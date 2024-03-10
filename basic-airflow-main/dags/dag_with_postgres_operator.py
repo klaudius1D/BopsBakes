735d4a9a8bc70e9742bf6caaedb344edfd06a897
Klaude Dungca <klaude.dungca@gmail.com>
2024-03-09 17:00:40 -0800

    fixed dag_with_postgres_operator.py

--- basic-airflow-main/dags/dag_with_postgres_operator.py
+++ basic-airflow-main/dags/dag_with_postgres_operator.py
@@ -1,28 +1,28 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.providers.postgres.operators.post import PosgresOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
-default_arg = {
-    'owner': 'Czar Klaudius',
-    'retries': 5,
-    'retry_delay': timedelta(minutes=5)
+default_args = {
+    "owner": "Czar Klaudius",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
 }
 
 with DAG(
-    dag_id='dag_with_postgres_operator_v1.0'
+    dag_id="dag_with_postgres_operator_v1.0",
     default_args=default_args,
     start_date=datetime(2021, 12, 19),
-    schedule_interval='0 0 * * *'
+    schedule_interval="0 0 * * *",
 ) as dag:
     task1 = PostgresOperator(
-        task_id='sample_query',
-        postgres_conn_id='postgres_default',
-         sql=''' 
+        task_id="sample_query",
+        postgres_conn_id="postgres_default",
+        sql=""" 
         CREATE TABLE IF NOT EXISTS dag_runs (
             dt date,
             dag_id character varying,
             primary key (dt, dag_id)
-        ''',
-    )  task1
+        """,
+    )
