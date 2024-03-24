a155268f64cc47eedd782350291f6db4ad4c59ff
Klaude Dungca <Klaude.Dungca@gmail.com>
2024-03-23 20:35:18 -0700

    created function to call client ID

--- basic-airflow-main/dags/payments_api.py
+++ basic-airflow-main/dags/payments_api.py
@@ -7,29 +7,33 @@ from square.http.auth.o_auth_2 import BearerAuthCredentials
 # print(config.square_access_token)
 # print(config.square_environment)
 
+
 # Configure your Square client
-client = Client(
-    bearer_auth_credentials=BearerAuthCredentials(
-        access_token=config.square_access_token
-    ),
-    environment=config.square_environment,
-)
+def client_call():
+    client = Client(
+        bearer_auth_credentials=BearerAuthCredentials(
+            access_token=config.square_access_token
+        ),
+        environment=config.square_environment,
+    )
+    return client
 
 
 # Example function to list payments
-# def list_payments():
-#     result = client.payments.list_payments(
-#         begin_time="2023-01-01T12:00:00Z",
-#         sort_order="desc",
-#         location_id="LMFGQ336T5DF1",
-#     )
+def list_payments():
+    client = client_call()
+    result = client.payments.list_payments(
+        begin_time="2023-01-01T12:00:00Z",
+        sort_order="desc",
+        location_id="LMFGQ336T5DF1",
+    )
 
-#     if result.is_success():
-#         print(result.body)
-#     elif result.is_error():
-#         print(result.errors)
+    if result.is_success():
+        print(result.body)
+    elif result.is_error():
+        print(result.errors)
 
 
-# # Calling the function
-# if __name__ == "__main__":
-#     list_payments()
+# Calling the function
+if __name__ == "__main__":
+    list_payments()
